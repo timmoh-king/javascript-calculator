a84f5da69d5b8c507cd2fa06848ac4ef94166bf7
timmoh-king <timothykariuki43@gmail.com>
2022-06-26 00:05:35 +0300

    First commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
+/**
+ * @Author: Your name
+ * @Date:   2022-06-26 00:03:32
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2022-06-26 00:04:21
+ */
 // *** Write your code here***
+const clear(){
+    
+}
