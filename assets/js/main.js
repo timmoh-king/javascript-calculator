cdae6745a3841680727da0d91201f1eb2eb32d33
timmoh-king <timothykariuki43@gmail.com>
2022-06-26 00:12:14 +0300

    js calc

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,8 @@
  * @Author: Your name
  * @Date:   2022-06-26 00:03:32
  * @Last Modified by:   Your name
- * @Last Modified time: 2022-06-26 00:04:21
+ * @Last Modified time: 2022-06-26 00:11:37
  */
 // *** Write your code here***
 const clear(){
-    
 }
