cf2042f322c34571fcf255571e7a02b9a22baef1
timmoh-king <timothykariuki43@gmail.com>
2022-06-26 00:30:09 +0300

    assignment

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
  * @Author: Your name
  * @Date:   2022-06-26 00:03:32
  * @Last Modified by:   Your name
- * @Last Modified time: 2022-06-26 00:17:24
+ * @Last Modified time: 2022-06-26 00:19:32
  */
 // *** Write your code here***
 const clear(){
