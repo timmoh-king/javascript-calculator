a10ed0ff65f244dd7142f9e901c6e1fc95e568b7
timmoh-king <timothykariuki43@gmail.com>
2022-06-27 23:58:05 +0300

    First changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,22 @@
  * @Author: Your name
  * @Date:   2022-06-26 00:03:32
  * @Last Modified by:   Your name
- * @Last Modified time: 2022-06-26 00:19:32
+ * @Last Modified time: 2022-06-27 23:56:29
  */
 // *** Write your code here***
-const clear(){
-    
+
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+  }
+
+  // The function clearNum() to clear on the values in the display
+function clearNum() {
+  if (form.displayResult.value != '') {     
+      form.displayResult.value = '';
+  }
+}
+
+// The function deleteNum() to delete a single value
+function delNum() {
+  form.displayResult.value = form.displayResult.value.substr(0, form.displayResult.value.length - 1);
 }
